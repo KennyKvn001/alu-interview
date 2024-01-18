93f3cbcd9c05b1e014119b8bfc4c44bda5452429
KennyKvn001 <k.mugisha@alustudent.com>
2024-01-18 13:52:44 -0800

    minimum operation

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" Minimumu operation, a method that calculates 
+    the fewest number of operations needed to result in exactly n H characters in the file.
+"""
+
+def minOperations(n):
+   
+    if n == 1:
+        return 0
+
+    result = 0
+    for i in range(2, n+1):
+        while n % i == 0:
+            result += i
+            n //= i
+    
+    return result
