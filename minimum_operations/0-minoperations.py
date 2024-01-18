ddfd3cba9d6bd17a4c2487d86a186653f610d43e
KvnKl007 <klakkent99@gmail.com>
2024-01-19 00:43:27 +0200

    update 0-minoperations.py file

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-""" Minimumu operation, a method that calculates 
-    the fewest number of operations needed to result 
+""" Minimumu operation, a method that calculates
+    the fewest number of operations needed to result
     in exactly n H characters in the file.
 """
 
