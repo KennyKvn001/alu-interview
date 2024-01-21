d9e94c509d1b8f9feb64e5c022589e8e8cb3e38b
KennyKvn007 <k.mugisha@alustudent.com>
2024-01-21 12:10:51 +0200

    updating 0-minoperations file

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -4,8 +4,8 @@
 
 def minOperations(n):
     """
-    Calculate the minimum number of operations required to obtain
-    a target integer from the input integer n.
+    Calculate and return the minimum number of operations
+    needed to result in exactly n H characters in the file.
     """
     if type(n) != int:
         return 0
@@ -14,6 +14,12 @@ def minOperations(n):
         return 0
 
     def prime_factors(num):
+        """
+        Calculate and return the prime factors of the given number.
+
+        :param num: int - the number for which to find prime factors
+        :return: list - a list of the prime factors of the input number
+        """
         result = []
         for i in range(2, int(num**0.5) + 1):
             while num % i == 0:
